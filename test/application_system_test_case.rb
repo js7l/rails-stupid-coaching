dfa605bf99b17eb125083b331fa7dcc195abf3ce
jessica wong <jsica7@gmail.com>
2022-02-27 17:04:22 +0800

    stupid_coaching

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :headless_chrome
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
 end
